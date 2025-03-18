efe56ba0248e0c529b9851877f2f338ead31acae
Raveen_lw <raveenlw44@gmail.com>
2025-03-19 01:49:52 +0530

    locationhelper-3

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -8,39 +8,48 @@ use App\Helpers\LocationHelper;
 class AttendanceController extends Controller
 {
     public function store(Request $request)
-    {
-        $request->validate([
-            'employee_id' => 'required|string',
-            'latitude' => 'required|numeric',
-            'longitude' => 'required|numeric',
-        ]);
+{
+    \Log::info('Attendance request received', $request->all()); // Log request
 
-        $officeLat = env('OFFICE_LATITUDE', 6.909634); // Default if env not set
-        $officeLon = env('OFFICE_LONGITUDE', 79.9669855);
-        $radius = env('RADIUS_METERS', 5);
+    $request->validate([
+        'employee_id' => 'required|string',
+        'latitude' => 'required|numeric',
+        'longitude' => 'required|numeric',
+    ]);
 
-        $distance = LocationHelper::getDistance(
-            $request->latitude,
-            $request->longitude,
-            $officeLat,
-            $officeLon
-        );
+    $officeLat = env('OFFICE_LATITUDE', 6.909634);
+    $officeLon = env('OFFICE_LONGITUDE', 79.9669855);
+    $radius = env('RADIUS_METERS', 10);
 
-        if ($distance > $radius) {
-            return response()->json([
-                'error' => true,
-                'message' => 'You are outside the allowed attendance area.'
-            ], 403);
-        }
+    $distance = LocationHelper::getDistance(
+        $request->latitude,
+        $request->longitude,
+        $officeLat,
+        $officeLon
+    );
+
+    if ($distance > $radius) {
+        \Log::info('Employee outside allowed area', ['distance' => $distance]);
         
-        $attendance = Attendance::create([
-            'employee_id' => $request->employee_id,
-            'latitude' => $request->latitude,
-            'longitude' => $request->longitude,
-        ]);
-
-        return response()->json(['message' => 'Attendance recorded!', 'data' => $attendance], 201);
+        return response()->json([
+            'error' => true,
+            'message' => 'You are outside the allowed attendance area.'
+        ], 403);
     }
+
+    $attendance = Attendance::create([
+        'employee_id' => $request->employee_id,
+        'latitude' => $request->latitude,
+        'longitude' => $request->longitude,
+    ]);
+
+    \Log::info('Attendance recorded successfully', $attendance->toArray());
+
+    return response()->json([
+        'message' => 'Attendance recorded!',
+        'data' => $attendance
+    ], 201);
+}
 
     public function index()
     {
