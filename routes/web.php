5035f7bc48fb25d1683a164470c0e3d9ffc5fc73
Raveen_lw <raveenlw44@gmail.com>
2025-03-18 20:46:03 +0530

    basic api-api files added

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\AttendanceController;
-
-Route::post('/attendance', [AttendanceController::class, 'store']);
-Route::get('/attendance', [AttendanceController::class, 'index']);
-
 use App\Http\Controllers\AttendanceViewController;
 
 Route::get('/attendance/view', [AttendanceViewController::class, 'index']);
