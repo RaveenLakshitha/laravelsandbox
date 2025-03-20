bcdfa7451e6ab60a5bde3b8d2950d3800090e7c5
Raveen_lw <raveenlw44@gmail.com>
2025-03-20 23:01:30 +0530

    new-implemantation-3

--- routes/api.php
+++ routes/api.php
@@ -33,4 +33,4 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 
-Route::get('/', [AttendanceController::class, 'index']);
+Route::get('/attendance', [AttendanceController::class, 'index']);
